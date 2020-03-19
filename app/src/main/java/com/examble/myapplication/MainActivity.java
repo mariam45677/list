a40aa8d0fdb8e8b304eed8cb8554786fe790f095
Bread traversy <mar@breadtraversy.com>
2020-03-19 16:00:59 +0200

    first commit

---
+++ app/src/main/java/com/examble/myapplication/MainActivity.java
@@ -0,0 +1,29 @@
+package com.examble.myapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.Menu;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+
+
+
+
+
+
+
+
+
+
+        }
+
